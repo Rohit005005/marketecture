b532592afd14518f1a28b58b2b4f6751ba0f677e
Ayush <ayushkumar605575@gmail.com>
2024-08-19 22:15:20 +0530

    About Us Page

---
+++ app/AboutUs/Navbar.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React from "react";
+import Link from 'next/link';
+
+
+function Navbar() {
+  return (
+    <div className="bg-white py-2 px-10 w-[90%] mx-auto rounded-b-[50px] flex justify-between items-center">
+      <div className="w-[93px] h-[70px] flex justify-center items-center">
+        <Image src={'/logo.png'} width={900} height={100} />
+      </div>
+      <div className="flex justify-between items-center gap-10">
+        <div className="text-black flex gap-5 text-[16px]">
+          <Link href="/"><p>Home</p></Link>
+          <p>Case Studies</p>
+          <p>Services</p>
+          <p>Blog</p>
+          <p>Clients</p>
+          <Link href="/"><p><b>About Us</b></p></Link>
+          <Link href="/ContactUs"><p>Contact</p></Link>
+        </div>
+        <div>
+          <button className="bg-black text-white px-5 py-2 text-[16px] rounded-full">
+            Enquire Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ app/AboutUs/page.js
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import Footer from "../_components/Footer";
+export default function ContactUs() {
+    return (
+        <>
+            <Hero />
+
+            <Footer />
+        </>
+    );
+}
