59dd3fb4f409f3478a604f1ec079940687efddcd
Ayush <ayushkumar605575@gmail.com>
2024-08-21 23:06:44 +0530

    About Us Page updated

---
+++ app/AboutUs/Testimonials.js
@@ -0,0 +1,15 @@
+
+export default function AboutTestimonies() {
+    return (
+        <div>
+            <div className="ml-6">
+                <div className="justify-center items-center w-full">
+                    <h1 className="mt-10 text-4xl font-bold bg-gradient-to-r from-pink-300 to-purple-700 bg-clip-text inline-block text-transparent">TESTIMONIALS</h1>
+                </div>
+                <div className="justify-center items-center w-full">
+                    <h1 className="mt-1 mb-5 text-3xl font-bold">Don't <span className="bg-red-500 bg-clip-text inline-block text-transparent">Just take </span> Our Words For It!</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
--- app/AboutUs/page.js
+++ app/AboutUs/page.js
@@ -1,10 +1,14 @@
 import Hero from "./Hero";
 import Footer from "../_components/Footer";
+import Features from "./Features";
+import AboutTestimonies from "./Testimonials";
 export default function ContactUs() {
     return (
         <>
             <Hero />
             <div className="border-t border-gray-400"></div>
+            <Features />
+            <AboutTestimonies />
             <Footer />
         </>
     );
