64cf96923211569ce64df02cc93b30e66b424fbc
Ayush <ayushkumar605575@gmail.com>
2024-09-13 19:20:16 +0530

    Group Photos Updated

--- app/_components/GroupPhotos.js
+++ app/_components/GroupPhotos.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function GroupPhotos() {
   return (
-    <div className="flex flex-col justify-center items-center mt-10 overflow-x-hidden">
+    <div className="flex flex-col justify-center items-center mt-10 py-8 overflow-x-hidden">
       <div className="flex flex-col justify-center items-center gap-1">
         <h1 className="text-[15px] sm:text-[24px] font-[400]">#spintersported</h1>
         <h1 className="text-[24px] sm:text-[40px] text-red-500 font-[800]">
@@ -19,23 +19,23 @@ function GroupPhotos() {
           achievements.
         </p>
       </div>
-      <div className="hidden sm:flex gap-10 mt-10">
-        <Image src={"/group1.png"} width={300} height={200} />
-        <Image src={"/group2.png"} width={300} height={100} />
-        <Image src={"/group3.png"} width={300} height={200} />
+      <div className="hidden sm:flex gap-16 mt-10">
+        <Image className="rotate-[-17deg] rounded-lg  w-[225px] h-[186px]" src={"/image_1.png"} width={300} height={200} />
+        <Image className="rounded-lg w-[250px] h-[220px]" src={"/image_2.png"} width={300} height={100} />
+        <Image className="rotate-[17deg] rounded-lg w-[225px] h-[186px]" src={"/image_3.png"} width={300} height={200} />
       </div>
       <div className="flex sm:hidden gap-5 mt-10">
-        <Image src={"/group1.png"} width={120} height={100} />
-        <Image src={"/group2.png"} width={120} height={100} />
-        <Image src={"/group3.png"} width={120} height={100} />
+        <Image className="rotate-[-17deg] rounded-lg w-32 h-32" src={"/image_1.png"} width={120} height={100} />
+        <Image className="rounded-lg w-32 h-32" src={"/image_2.png"} width={120} height={100} />
+        <Image className="rotate-[17deg] rounded-lg w-32 h-32" src={"/image_3.png"} width={120} height={100} />
       </div>
-      <div className="hidden sm:flex gap-10 mt-10">
-        <Image src={"/group4.png"} width={300} height={200} />
-        <Image src={"/group5.png"} width={300} height={200} />
+      <div className="hidden sm:flex gap-20 mt-10">
+        <Image className="rotate-[17deg] rounded-lg w-[225px] h-[186px]" src={"/image_4.png"} width={300} height={200} />
+        <Image className="rotate-[-17deg] rounded-lg w-[225px] h-[186px]"  src={"/image_5.png"} width={300} height={200} />
       </div>
-      <div className="flex sm:hidden gap-5 mt-5">
-        <Image src={"/group4.png"} width={120} height={100} />
-        <Image src={"/group5.png"} width={120} height={100} />
+      <div className="flex sm:hidden gap-10 mt-5">
+        <Image className="rotate-[17deg] rounded-lg w-32 h-32" src={"/image_4.png"} width={120} height={100} />
+        <Image className="rotate-[-17deg] rounded-lg w-32 h-32" src={"/image_5.png"} width={120} height={100} />
       </div>
     </div>
   );
