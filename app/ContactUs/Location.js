b1430caa8490ba64f643d0ac89d835a6cbb85820
Rohit005005 <137618172+Rohit005005@users.noreply.github.com>
2024-09-16 20:35:17 +0530

    updated about and contact partially

--- app/ContactUs/Location.js
+++ app/ContactUs/Location.js
@@ -11,7 +11,7 @@ export default function Location() {
 
   return (
     <div className="relative flex flex-col items-center">
-      <div className="relative flex flex-col sm:flex-row items-center justify-center z-20">
+      {/*<div className="relative flex flex-col sm:flex-row items-center justify-center z-20">
         <Image
           src={"/svgs/address.svg"}
           alt="First Image"
@@ -30,6 +30,57 @@ export default function Location() {
           width="348"
           height="148"
         />
+      </div>*/}
+
+      <div className="relative flex flex-col sm:flex-row items-center justify-center z-20 gap-5 sm:gap-14 mb-10">
+        <div className="flex bg-[#FFFFFF] w-[300px] h-[120px]  justify-center items-center p-5 gap-5 rounded-lg shadow-lg">
+          <div className="w-[200px] flex ">
+          <Image
+            src={"/locLogo.png"}
+            alt="location icon"
+            width="100"
+            height="100"
+          />
+          </div>
+          <div>
+            <h1 className="text-[14px] font-[500]">Address:</h1>
+            <p className="text-[12px] font-[400]">
+            39, Paddana Reddy Layout Rd, Nanjappa Garden, Horamavu, Bengaluru, Karnataka 560043
+            </p>
+          </div>
+        </div>
+        <div className="flex bg-[#FFFFFF] w-[300px] h-[120px]  justify-start items-center p-5 gap-5 rounded-lg shadow-lg">
+          <div className="w-[65px] flex ">
+          <Image
+            src={"/phoneLogo.png"}
+            alt="location icon"
+            width="100"
+            height="100"
+          />
+          </div>
+          <div>
+            <h1 className="text-[14px] font-[500]">Phone:</h1>
+            <p className="text-[12px] font-[400]">
+            9900929710
+            </p>
+          </div>
+        </div>
+        <div className="flex bg-[#FFFFFF] w-[300px] h-[120px]  justify-start items-center p-5 gap-5 rounded-lg shadow-lg">
+          <div className="w-[65px] flex ">
+          <Image
+            src={"/emailLogo.png"}
+            alt="location icon"
+            width="100"
+            height="100"
+          />
+          </div>
+          <div>
+            <h1 className="text-[14px] font-[500]">Email:</h1>
+            <p className="text-[12px] font-[400]">
+            info@marketecture.in
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="relative w-full h-[300px] flex justify-center overflow-hidden mt-[-108px] z-10">
@@ -43,7 +94,7 @@ export default function Location() {
               marginHeight="0"
               marginWidth="0"
               className="border-none -mt-[150px]"
-              src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=en&amp;q=13%C2%B001'12.8%22N%2077%C2%B039'20.0%22E+(Marketecture)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+              src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=en&amp;q=39,%20Paddana%20Reddy%20Layout%20Rd,%20Nanjappa%20Garden,%20Horamavu,%20Bengaluru,%20Karnataka%20560043+(Marketecture)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
             >
               <a href="https://www.gps.ie/">gps vehicle tracker</a>
             </iframe>
