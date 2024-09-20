fb49a0f0624f02c25b9dbb0fde3e6f683aae9eac
Ayush <ayushkumar605575@gmail.com>
2024-09-20 22:43:05 +0530

    Text style updated

--- app/_components/Marketing.js
+++ app/_components/Marketing.js
@@ -1,6 +1,14 @@
+"use client";
+
+import { useState } from 'react';
 import PropTypes from "prop-types";
 
 export default function Marketing() {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleText = () => {
+        setIsExpanded(!isExpanded);
+    };
     return (
         <div className={` w-[1440px] max-w-full overflow-hidden items-end justify-start py-20 px-[13px] box-border relative gap-[57px] leading-[normal] tracking-[normal] text-left text-[40px] text-black font-body-1-bold ${PropTypes.string}`}>
             <div className="items-start justify-start">
@@ -8,7 +16,12 @@ export default function Marketing() {
                     <h1 className="text-[40px] py-08 font-[700] italic">
                         Marketing
                     </h1>
-                    <div className="text-[20px] py-10 font-[400] leading-8">
+                    <div
+                        className={`text-[20px] py-10 font-[400] leading-8 ${isExpanded ? '' : 'line-clamp-2'}`}
+                        style={{
+                            height: isExpanded ? 'auto' : '108px', // Approx. 2 lines height in pixels, adjust based on font-size
+                        }}
+                    >
                         Our method blends data analysis with creative insight to craft personalized strategies that
                         captivate your target audience, enhancing engagement, boosting conversions, and fostering
                         enduring brand loyalty.
@@ -17,9 +30,10 @@ export default function Marketing() {
                 <div className="w-[633px] flex flex-col items-center justify-center gap-6 max-w-full">
                     <div className="items-center justify-center">
                         <button
-                            className={`cursor-pointer border-[#000] border-[1px] border-solid py-[14px] px-[36px] bg-[transparent] rounded-[32px] flex items-center justify-center hover:bg-[rgba(51,51,51,0.09)] hover:border-[#333] hover:border-[1px] hover:border-solid hover:box-border`}
+                            onClick={toggleText}
+                            className={`cursor-pointer hover:underline focus:outline-none border-[#000] border-[1px] border-solid py-[14px] px-[36px] bg-[transparent] rounded-[32px] flex items-center justify-center hover:bg-[rgba(51,51,51,0.09)] hover:border-[#333] hover:border-[1px] hover:border-solid hover:box-border`}
                         >
-                            <b className="text-[16px] font-[500] font-bold ">See More</b>
+                            <b className="text-[16px] font-[500] font-bold ">{isExpanded ? 'Show Less' : 'Show More'}</b>
                         </button>
                     </div>
                 </div>
