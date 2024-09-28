c6e9644f0167472feac569d35ed47a45b6314835
Rohit005005 <137618172+Rohit005005@users.noreply.github.com>
2024-09-28 20:12:45 +0530

    added infinity scrolling sections

--- app/_components/GroupPhotos.js
+++ app/_components/GroupPhotos.js
@@ -5,37 +5,270 @@ function GroupPhotos() {
   return (
     <div className="flex flex-col justify-center items-center mt-10 py-8 overflow-x-hidden">
       <div className="flex flex-col justify-center items-center gap-1">
-        <h1 className="text-[15px] sm:text-[24px] font-[400]">#spintersported</h1>
+        <h1 className="text-[15px] sm:text-[24px] font-[400]">
+          #spintersported
+        </h1>
         <h1 className="text-[24px] sm:text-[40px] text-red-500 font-[800]">
           Garnering accolades,
         </h1>
-        <h1 className="text-[24px] sm:text-[40px] italic font-[800]">Sharing expertise</h1>
+        <h1 className="text-[24px] sm:text-[40px] italic font-[800]">
+          Sharing expertise
+        </h1>
       </div>
       <div className="flex text-center w-[98%] sm:w-[60%] mt-5 sm:mt-10">
         <p className="text-[12px] sm:text-[20px] font-[400]">
-          Through our participation, we’ve had the privilege of being acknowledged for our
-          contributions, receiving honours through various awards. These moments have deeply
-          enriched us, enabling us to network with other professionals and celebrate shared
-          achievements.
+          Through our participation, we’ve had the privilege of being
+          acknowledged for our contributions, receiving honours through various
+          awards. These moments have deeply enriched us, enabling us to network
+          with other professionals and celebrate shared achievements.
         </p>
       </div>
-      <div className="hidden sm:flex gap-16 mt-10">
-        <Image className="rotate-[-17deg] rounded-lg  w-[225px] h-[186px]" src={"/image_1.png"} width={300} height={200} />
-        <Image className="rounded-lg w-[250px] h-[220px]" src={"/image_2.png"} width={300} height={100} />
-        <Image className="rotate-[17deg] rounded-lg w-[225px] h-[186px]" src={"/image_3.png"} width={300} height={200} />
+      {/* <div className="hidden sm:flex gap-16 mt-10">
+        <Image
+          className="rotate-[-17deg] rounded-lg  w-[225px] h-[186px]"
+          src={"/image_1.png"}
+          width={300}
+          height={200}
+        />
+        <Image
+          className="rounded-lg w-[250px] h-[220px]"
+          src={"/image_2.png"}
+          width={300}
+          height={100}
+        />
+        <Image
+          className="rotate-[17deg] rounded-lg w-[225px] h-[186px]"
+          src={"/image_3.png"}
+          width={300}
+          height={200}
+        />
       </div>
       <div className="flex sm:hidden gap-5 mt-10">
-        <Image className="rotate-[-17deg] rounded-lg w-32 h-32" src={"/image_1.png"} width={120} height={100} />
-        <Image className="rounded-lg w-32 h-32" src={"/image_2.png"} width={120} height={100} />
-        <Image className="rotate-[17deg] rounded-lg w-32 h-32" src={"/image_3.png"} width={120} height={100} />
+        <Image
+          className="rotate-[-17deg] rounded-lg w-32 h-32"
+          src={"/image_1.png"}
+          width={120}
+          height={100}
+        />
+        <Image
+          className="rounded-lg w-32 h-32"
+          src={"/image_2.png"}
+          width={120}
+          height={100}
+        />
+        <Image
+          className="rotate-[17deg] rounded-lg w-32 h-32"
+          src={"/image_3.png"}
+          width={120}
+          height={100}
+        />
       </div>
       <div className="hidden sm:flex gap-20 mt-10">
-        <Image className="rotate-[17deg] rounded-lg w-[225px] h-[186px]" src={"/image_4.png"} width={300} height={200} />
-        <Image className="rotate-[-17deg] rounded-lg w-[225px] h-[186px]"  src={"/image_5.png"} width={300} height={200} />
+        <Image
+          className="rotate-[17deg] rounded-lg w-[225px] h-[186px]"
+          src={"/image_4.png"}
+          width={300}
+          height={200}
+        />
+        <Image
+          className="rotate-[-17deg] rounded-lg w-[225px] h-[186px]"
+          src={"/image_5.png"}
+          width={300}
+          height={200}
+        />
       </div>
       <div className="flex sm:hidden gap-10 mt-5">
-        <Image className="rotate-[17deg] rounded-lg w-32 h-32" src={"/image_4.png"} width={120} height={100} />
-        <Image className="rotate-[-17deg] rounded-lg w-32 h-32" src={"/image_5.png"} width={120} height={100} />
+        <Image
+          className="rotate-[17deg] rounded-lg w-32 h-32"
+          src={"/image_4.png"}
+          width={120}
+          height={100}
+        />
+        <Image
+          className="rotate-[-17deg] rounded-lg w-32 h-32"
+          src={"/image_5.png"}
+          width={120}
+          height={100}
+        />
+      </div> */}
+      <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)] mt-10">
+        <ul className="flex items-center justify-center md:justify-start [&_li]:mx-5 [&_img]:max-w-none animate-infinite-scroll">
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_1.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_2.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_3.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_4.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_5.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+        </ul>
+        <ul
+          className="flex items-center justify-center md:justify-start [&_li]:mx-5 [&_img]:max-w-none animate-infinite-scroll"
+          aria-hidden="true"
+        >
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_1.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_2.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_3.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_4.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_5.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+        </ul>
+      </div>
+
+      <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)] mt-10">
+        <ul className="flex items-center justify-center md:justify-start [&_li]:mx-5 [&_img]:max-w-none animate-infinite-scroll2">
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_1.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_2.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_3.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_4.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_5.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+        </ul>
+        <ul
+          className="flex items-center justify-center md:justify-start [&_li]:mx-5 [&_img]:max-w-none animate-infinite-scroll2"
+          aria-hidden="true"
+        >
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_1.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_2.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_3.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_4.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+          <li>
+            <Image
+              className=" w-56 h-56"
+              src={"/image_5.png"}
+              width={120}
+              height={100}
+            />
+          </li>
+        </ul>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,122 +1,133 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		backgroundImage: {
-  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-  		},
-  		keyframes: {
-  			typing: {
-  				'0%': {
-  					width: '0%',
-  					visibility: 'hidden'
-  				},
-  				'100%': {
-  					width: '100%'
-  				}
-  			},
-  			blink: {
-  				'50%': {
-  					borderColor: 'transparent'
-  				},
-  				'100%': {
-  					borderColor: 'black'
-  				}
-  			},
-  			text: {
-  				'0%': {
-  					visibility: 'hidden'
-  				},
-  				'100%': {
-  					visibility: 'hidden'
-  				}
-  			},
-  			box: {
-  				'0%': {
-  					width: '40px',
-  					scale: '0'
-  				},
-  				'50%': {
-  					width: '40px',
-  					scale: '0'
-  				},
-  				'70%': {
-  					width: '40px',
-  					scale: '1'
-  				},
-  				'100%': {
-  					width: '238px'
-  				}
-  			}
-  		},
-  		animation: {
-  			typing: 'typing 2s steps(20) infinite alternate, blink .7s infinite',
-  			typing2: 'typing 2s 2s alternate, text 2s',
-  			typing3: 'typing 2s 2.5s alternate, text 2.5s',
-  			typing4: 'typing 2s 3s alternate, text 3s',
-  			typing5: 'typing 2s 3.5s alternate, text 3.5s',
-  			typing6: 'typing 2s 4s alternate, text 4s',
-  			box: 'box 2s alternate',
-  			box3: 'box 2.5s alternate',
-  			box4: 'box 3s alternate',
-  			box5: 'box 3.5s alternate',
-  			box6: 'box 4s alternate'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          "100%": {
+            borderColor: "black",
+          },
+        },
+        text: {
+          "0%": {
+            visibility: "hidden",
+          },
+          "100%": {
+            visibility: "hidden",
+          },
+        },
+        box: {
+          "0%": {
+            width: "40px",
+            scale: "0",
+          },
+          "50%": {
+            width: "40px",
+            scale: "0",
+          },
+          "70%": {
+            width: "40px",
+            scale: "1",
+          },
+          "100%": {
+            width: "238px",
+          },
+        },
+		'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+		'infinite-scroll2': {
+          from: { transform: 'translateX(-100%)' },
+          to: { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite",
+        typing2: "typing 2s 2s alternate, text 2s",
+        typing3: "typing 2s 2.5s alternate, text 2.5s",
+        typing4: "typing 2s 3s alternate, text 3s",
+        typing5: "typing 2s 3.5s alternate, text 3.5s",
+        typing6: "typing 2s 4s alternate, text 4s",
+        box: "box 2s alternate",
+        box3: "box 2.5s alternate",
+        box4: "box 3s alternate",
+        box5: "box 3.5s alternate",
+        box6: "box 4s alternate",
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+		"infinite-scroll2":"infinite-scroll2 25s linear infinite"
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
